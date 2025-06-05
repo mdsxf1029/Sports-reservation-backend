78e5ed97d3ccc723c430b72fa9b3549d96438cb2
Zhang Xiang <2352985@tongji.edu.cn>
2025-06-05 20:37:50 +0800

    Refactor(main)：对Program文件（入口文件）进行了重构
    删除项目新建时自带的接口模式，
    新增基于swagger的API测试（需要新增Nuget包），
    对测试页面的格式进行了调整（网页的标题，网站内的提示字符）

--- Program.cs
+++ Program.cs
@@ -1,41 +1,39 @@
+using Microsoft.OpenApi.Models;
+
+// 创建web应用构建器
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers(); // 添加服务到容器
 
+builder.Services.AddSwaggerGen(c => 
+    {
+        c.SwaggerDoc("api" ,new OpenApiInfo
+        {
+            Title = "运动场地预约系统 | 数据库网络应用程序接口 | Database Web API",
+            Description = """
+                          欢迎来到我们的运动场地预约系统。在这里你可以浏览我们的数据库网络应用程序。
+                          """
+        });
+    }
+);
+
+// 构建web服务
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseDeveloperExceptionPage(); // 启用开发者异常画面
 }
 
-app.UseHttpsRedirection();
+app.UseSwagger(); // 启用swagger中间件
+app.UseSwaggerUI(c => // 启用swaggerUI
+    {
+        c.SwaggerEndpoint("/swagger/api/swagger.json", "api");
+        c.RoutePrefix = string.Empty; 
+        c.DocumentTitle = "运动场地预约系统 - API";
+    }
+);
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+app.UseHttpsRedirection(); // 启动HTTPS重定向中间件 
+app.MapControllers(); // 将控制器映射到路由
+app.Run(); // 启动应用程序并开始处理请求
