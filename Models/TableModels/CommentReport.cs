88d98cb8d136e0b38553d0ccd7567548780ad332
Zhang Xiang <2352985@tongji.edu.cn>
2025-06-08 22:17:45 +0800

    补充了一些表结构模型

--- Models/TableModels/CommentReport.cs
+++ Models/TableModels/CommentReport.cs
@@ -1,4 +1,8 @@
-﻿namespace Sports_reservation_backend.Models.TableModels;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Sports_reservation_backend.Models.TableModels;
 
 public class CommentReport
 {
